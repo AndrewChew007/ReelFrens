23a8921717cc7196ad5a5a7829eddeeca6e19b58
AndrewChew007 <82247217+AndrewChew007@users.noreply.github.com>
2026-03-23 04:15:52 +0800

    Firebase data collection functionality added

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+apiKey: "AIzaSyDZhiHT15iDFEvVUj7UHOyk6mnTKHc0c_8",
+authDomain: "reelfrens.firebaseapp.com",
+projectId: "reelfrens",
+storageBucket: "reelfrens.firebasestorage.app",
+messagingSenderId: "312853025037",
+appId: "1:312853025037:web:b691179a5589b479816044"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+
 document.querySelectorAll(".card").forEach((card)=>{
     
     const header = card.querySelector(".card_header");
@@ -29,4 +50,41 @@ document.querySelectorAll(".volume_container").forEach((container)=>{
     decrease.addEventListener("click", ()=>{
         input.stepDown();
     });
+});
+
+const submit = document.querySelector("#submit_button");
+
+submit.addEventListener("click", async (e)=>{
+    e.preventDefault();
+
+    const data = {
+        cards: {}
+    };
+
+    document.querySelectorAll(".card").forEach((card)=>{
+        const checkbox = card.querySelector(".card_checkbox");
+        const title = card.querySelector(".card_title");
+
+        data.cards[title.textContent] = checkbox.checked;
+    });
+
+    const number = document.querySelector("#volume_number");
+
+    data["volume_number"] = number.value;
+
+    const handle = document.querySelector(".submit_container input");
+
+    data["IG_handle"] = handle.value;
+
+    console.log(data)
+
+    // Send to Firebase
+    try {
+        await addDoc(collection(db, "responses"), data);
+        console.log("Data saved onto Firebase");
+        alert("Response Saved!")
+        submit.disabled = true;
+    } catch (error) {
+        console.error("Error saving data:", error);
+    }
 });
